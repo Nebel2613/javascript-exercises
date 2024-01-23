e54641bc1a076abcb1aad432bc52c3f76842b873
Nebel2613 <nebel2613@gmail.com>
2024-01-23 13:55:37 +0100

    05 passed test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+    if (a < 0 || b < 0) {return "ERROR"}
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    while (min <= max) {
+        sum += min;
+        min += 1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
